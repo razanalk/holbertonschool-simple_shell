26d3051bf7fd7abf086def2836c289b3d7eabdb3
Mayamner <mem.kpope@gmail.com>
2026-05-12 20:01:07 +0300

    fixing the main.c file

--- main.c
+++ main.c
@@ -1,18 +1,39 @@
 #include "shell.h"
 
 /**
- * main - Entry point
- *
+ * trim - Removes leading and trailing spaces
+ * @str: String to trim
+ * Return: Trimmed string
+ */
+char *trim(char *str)
+{
+	char *end;
+
+	while (*str == ' ' || *str == '\t')
+		str++;
+	if (*str == '\0')
+		return (str);
+	end = str + strlen(str) - 1;
+	while (end > str && (*end == ' ' || *end == '\t'))
+		end--;
+	*(end + 1) = '\0';
+	return (str);
+}
+
+/**
+ * main - Entry point for the simple shell
  * Return: Always 0
  */
 int main(void)
 {
 	char *line = NULL;
+	char *trimmed;
 	size_t len = 0;
 	ssize_t read;
-	char *argv[2];
+	char *argv[3];
 	pid_t pid;
 	int status;
+	char *space;
 
 	while (1)
 	{
@@ -27,17 +48,30 @@ int main(void)
 		}
 
 		line[read - 1] = '\0';
+		trimmed = trim(line);
 
-		if (line[0] == '\0')
+		if (trimmed[0] == '\0')
 			continue;
 
-		argv[0] = line;
-		argv[1] = NULL;
+		space = strchr(trimmed, ' ');
+		if (space)
+		{
+			*space = '\0';
+			argv[0] = trimmed;
+			argv[1] = trim(space + 1);
+			argv[2] = NULL;
+		}
+		else
+		{
+			argv[0] = trimmed;
+			argv[1] = NULL;
+			argv[2] = NULL;
+		}
 
 		pid = fork();
 		if (pid == 0)
 		{
-			if (execve(line, argv, environ) == -1)
+			if (execve(argv[0], argv, environ) == -1)
 			{
 				perror("./shell");
 				free(line);
