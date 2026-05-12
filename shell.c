1e8d3bf277e58b8cfca846c83d79444e13e8ad10
Mayamner <mem.kpope@gmail.com>
2026-05-12 19:10:25 +0300

    adding simple shell 0.1

---
+++ shell.c
@@ -0,0 +1,53 @@
+#include "shell.h"
+
+/**
+ * main - Entry point for the simple shell
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t read;
+	char *argv[2];
+	pid_t pid;
+	int status;
+
+	while (1)
+	{
+		if (isatty(STDIN_FILENO))
+			write(STDOUT_FILENO, "#cisfun$ ", 9);
+
+		read = getline(&line, &len, stdin);
+		if (read == -1)
+		{
+			free(line);
+			exit(0);
+		}
+
+		line[read - 1] = '\0';
+
+		if (line[0] == '\0')
+			continue;
+
+		argv[0] = line;
+		argv[1] = NULL;
+
+		pid = fork();
+		if (pid == 0)
+		{
+			if (execve(line, argv, environ) == -1)
+			{
+				perror("./shell");
+				free(line);
+				exit(1);
+			}
+		}
+		else
+			waitpid(pid, &status, 0);
+	}
+
+	free(line);
+	return (0);
+}
